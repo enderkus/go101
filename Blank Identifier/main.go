1af816557fe8c1e1edc08a4511657704f90659dd
enderkus <enderkus@msn.com>
2022-05-24 14:54:08 +0300

    Boş tanımlayıcı eklemeleri yapıldı.

---
+++ Blank Identifier/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	var _ = 22
+
+	/*
+
+		Boş tanımlayıcılar fonksiyona parametreleri boş göndermek için kullanılır.
+		Boş değişkenleri yazdırmak için çağıramazsın.
+
+	*/
+}
