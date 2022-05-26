f7aed9d9c055a14bf4bd9ee2ce0fd01ecf4b8a23
enderkus <enderkus@msn.com>
2022-05-26 13:10:40 +0300

    Örnek desi hesaplama projesi oluşturuldu.

---
+++ Ornek-Desi-Hesaplama/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("### Desi Hesaplama Programı ###")
+	fmt.Println("Lütfen ondalıklı değerleri nokta ile ayırın.")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Lütfen en değeri giriniz (Cm cinsinden) : ")
+	scanner.Scan()
+	en, _ := strconv.ParseFloat(scanner.Text(), 64)
+	fmt.Print("Lütfen boy değeri giriniz (Cm cinsnden ) : ")
+	scanner.Scan()
+	boy, _ := strconv.ParseFloat(scanner.Text(), 64)
+	fmt.Print("Lütfen yükseklik değeri giriniz (Cm cinsnden ) : ")
+	scanner.Scan()
+	yukseklik, _ := strconv.ParseFloat(scanner.Text(), 64)
+	DesiHesapla := (en * boy * yukseklik) / 3000
+	fmt.Printf("Kargonuzun desisi : %2.f", DesiHesapla)
+}
